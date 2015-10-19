14e8b36b0dfe9c81d6660f855edee7242ba5242e
Lesley Antwi <lpantwi760@gmail.com>
2015-10-19 00:18:27 -0400

    last update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Game
+# Connect Four
+
+Connect Four is similar to how Tic Tac Toe is played just with more columns and rows, I use the TTT     homework as a reference point.
+
+In order to produce a DRY code you have to use constructors else I would have 42 different functions represented each cell.
+
+My plan was I asked each object what do I need you to produce. For instance the disk constructor, I needed it to check for value than give the disk a value.
+
+So forth I did the same thing with the table, I asked it what do I need you to produce. I needed my table to store and track the moves.
+
+Finally when I reach the game function I was able to call back my methods and functions.
+
+I ran into trouble when trying to add a click event, I think one of the methods did not flow correctly through the event.
+
+Having trouble with the click event tricked down to me having trouble with finding the winner. Since it kind of impossible to write out each winner combination, I was trying figure out a way to run  the columns through a for loop than compare values in sets of 4.
+
+
+
+https://trello.com/b/z8sXRTSP/connect-four
--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-$(function() {
+$(document).ready(function() {
   console.log("Loaded");
   var p1name = prompt("Please enter your name");
   var p2name = prompt("Please enter your name");
 
+  $('.disk').on('click',function() {
+   var col = $(this).attr('id');
+   game.play(col);
+  })
 
-  $('.disk').on('click', Table);
-
-});
+})
 
 
    //Disk contructor setting value equal null if so return value of disk
@@ -19,8 +21,8 @@ $(function() {
      return this.value;
      } else {
      alert ("choose another move");
-    }
-
+     }
+   }
       Disk.prototype.setColor= function () {
       if(this.currentPlayer) {
       return (this.currentPlayer = $('#id').css('background-color','black'))
@@ -29,58 +31,43 @@ $(function() {
       this. currentPlayer =$('#id').css('background-color','black');
 
       }
-  };
-
+  }
       var Table = function (){
                   this.disks = [];
-       }
-
+                  this.winner = null;
+  }
       Table.prototype.settingDisk = function (){
       $('.disk').forEach(function(cell){
                    this.disks.push(new Disk());
-
-      });
+      })
    }
 
        Table.prototype.changeColor = function (plot) {
          var moveDisk = this.disks[plot];
          if (moveDisk && moveDisk.playable()) {
-              return moveDisk.setColor;
+            return moveDisk.setColor;
       }
-};
+
+      Table.prototype.checkWinner = function() {
+          var columnList = document.querySelectorAll('.column')
+          for(var i = 0; i < column.length; i++);
+          if (columnList[i] === )
 
 
+     });
+  }
+      var game = {
+       table :'',
+       makeTable: function () {
+               this.table = new Table();
 
+      },
+      start: function () {
+             this.makeTable();
+             this.table.settingDisk();
+      },
+      play: function(plot) {
+          this.table.changeColor(plot);
+     }
 
-
-
-
-
-/*  Table.prototype.checkrow = function() {
-    var rowA,rowB,rowC,rowD,rowE,rowF;
-    var colA,colB,colC,colD,colE,colF,colG;
-
-  rowA = [$('#1'),$('#2'),$('#3'),$('#4'),$('#5'),$('#6'),('#7')];
-  rowB = [$('#8'),$('#9'),$('#10'),$('#11'),$('#12'),$('#13'),('#14')];
-  rowC = [$('#15'),$('#16'),$('#17'),$('#18'),$('#19'),$('#20'),('#21')];
-  rowD = [$('#22'),$('#23'),$('#24'),$('#25'),$('#26'),$('#27'),('#28')];
-  rowE = [$('#29'),$('#30'),$('#31'),$('#32'),$('#33'),$('#34'),('#35')];
-  rowF = [$('#36'),$('#37'),$('#38'),$('#39'),$('#40'),$('#41'),('#42')];
-
-  colA = [$('#1'),$('#8'),$('#15'),$('#22'),$('#29'),$('#36')];
-  colB = [$('#2'),$('#9'),$('#16'),$('#23'),$('#30'),$('#37')];
-  colC = [$('#3'),$('#10'),$('#17'),$('#24'),$('#31'),$('#38')];
-  colD = [$('#4'),$('#11'),$('#18'),$('#25'),$('#32'),$('#39')];
-  colE = [$('#5'),$('#12'),$('#19'),$('#26'),$('#33'),$('#40')];
-  colF = [$('#6'),$('#13'),$('#20'),$('#27'),$('#30'),$('#41')];
-  colG = [$('#7'),$('#14'),$('#21'),$('#28'),$('#31'),$('#42')];
-
-});
-
-Table .prototype.newDisk =
-
-//
-// playtime
-var currentPlayer:
-mydisc = new Disk
-mytable = new Table */
+  };
